50aba1e95d46c22e6d27461530ccc2536c159dfc
nitheeshcpu <nitishkoda@outlook.com>
2022-11-02 21:36:32 -0500

    added new theme

---
+++ arch-rices-main-nord/arch-rices-main-nord/nord/polybar/nord/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar mid -c $HOME/.config/polybar/nord/config.ini &
+polybar left -c $HOME/.config/polybar/nord/config.ini &
+polybar power -c $HOME/.config/polybar/nord/config.ini &
+polybar right -c $HOME/.config/polybar/nord/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+	polybar external -c $(dirname $0)/config.ini &
+fi
---
+++ arch-rices-main-nord/arch-rices-main-nord/nord/rofi/powermenu/shared/theme.bash
@@ -0,0 +1,4 @@
+## Current Theme
+
+type="$HOME/.config/rofi/powermenu/type-1"
+style='nord.rasi'
